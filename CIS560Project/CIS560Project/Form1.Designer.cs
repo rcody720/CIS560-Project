4042f9091aa889b27b9120efb333b36c4d54ab53
rcody720 <rcody720@ksu.edu>
2021-04-14 20:30:55 -0500

    Starting the UI

---
+++ CIS560Project/CIS560Project/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace CIS560Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uxSearchButton = new System.Windows.Forms.Button();
+            this.uxTopTenListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(45, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(677, 49);
+            this.textBox1.TabIndex = 0;
+            // 
+            // uxSearchButton
+            // 
+            this.uxSearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSearchButton.Location = new System.Drawing.Point(794, 50);
+            this.uxSearchButton.Name = "uxSearchButton";
+            this.uxSearchButton.Size = new System.Drawing.Size(200, 72);
+            this.uxSearchButton.TabIndex = 1;
+            this.uxSearchButton.Text = "Search";
+            this.uxSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // uxTopTenListView
+            // 
+            this.uxTopTenListView.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxTopTenListView.HideSelection = false;
+            this.uxTopTenListView.Location = new System.Drawing.Point(701, 177);
+            this.uxTopTenListView.Name = "uxTopTenListView";
+            this.uxTopTenListView.Size = new System.Drawing.Size(292, 518);
+            this.uxTopTenListView.TabIndex = 2;
+            this.uxTopTenListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 746);
+            this.Controls.Add(this.uxTopTenListView);
+            this.Controls.Add(this.uxSearchButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button uxSearchButton;
+        private System.Windows.Forms.ListView uxTopTenListView;
+    }
+}
+
