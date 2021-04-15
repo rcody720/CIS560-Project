689d605af724e02fa78cc19b6975f66e685cd680
Tayler Stoppel <stoppel@ksu.edu>
2021-04-14 20:52:26 -0500

    Starter sql table creation file

---
+++ Tables.sql
@@ -0,0 +1,60 @@
+CREATE SCHEMA [Movies] AUTHORIZATION [dbo];
+GO
+
+DROP TABLE IF EXISTS Movies.Movie
+DROP TABLE IF EXISTS Movies.Theater
+DROP TABLE IF EXISTS Movies.Director
+DROP TABLE IF EXISTS Movies.Actor
+DROP TABLE IF EXISTS Movies.Genre
+DROP TABLE IF EXISTS Movies.Reviewer
+DROP TABLE IF EXISTS Movies.MovieGenre
+DROP TABLE IF EXISTS Movies.MovieReview
+DROP TABLE IF EXISTS Movies.MovieTheater
+DROP TABLE IF EXISTS Movies.MovieDirector
+DROP TABLE IF EXISTS Movies.MovieActor
+
+CREATE TABLE Movies.Movie
+(
+    MovieId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    MovieTitle NVARCHAR(128) NOT NULL,
+    Rating FLOAT,
+    RuntimeMinutes INT,
+    ReleaseDate DATE
+)
+
+CREATE TABLE Movies.Theater
+(
+    TheaterId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    TicketPrice FLOAT NOT NULL
+)
+
+CREATE TABLE Movies.Genre
+(
+    GenreId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    GenreName NVARCHAR(32)
+)
+
+CREATE TABLE Movies.Director
+(
+    DirectorId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    FirstName NVARCHAR(32) NOT NULL,
+    LastName NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Movies.Actor
+(
+    ActorId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    FirstName NVARCHAR(32) NOT NULL,
+    LastName NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Movies.Reviewer
+(
+    ReviewerId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [Username] NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE Movies.MovieGenre
+(
+    
+)
