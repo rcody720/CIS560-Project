2b2f4bb1a6bb2030748bcc0352d312d3f21c9c22
Cody Reeves <rcody720@ksu.edu>
2021-04-24 16:51:19 -0500

    Connected CreateMovie to the database

--- CIS560Project/CIS560Project/SqlMovieRepository.cs
+++ CIS560Project/CIS560Project/SqlMovieRepository.cs
@@ -4,14 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CIS560Project.DataDelegates;
+using DataAccess;
 
 namespace CIS560Project
 {
     public class SqlMovieRepository : IMovieRepository
     {
-        public Movie CreateMovie(int movieId, string movieName, string rating, int runTime, DateTime releaseDate)
+
+        private readonly SqlCommandExecutor executor;
+
+        public SqlMovieRepository(string connectionString)
         {
-            throw new NotImplementedException();
+            executor = new SqlCommandExecutor(connectionString);
+        }
+
+        public Movie CreateMovie(string movieName, string rating, int runTime, DateTime releaseDate)
+        {
+            if (string.IsNullOrWhiteSpace(movieName))
+                throw new ArgumentException("The parameter cannot be null or empty.", nameof(movieName));
+
+            if (string.IsNullOrWhiteSpace(rating))
+                throw new ArgumentException("The parameter cannot be null or empty.", nameof(rating));
+
+            if (runTime == 0)
+                throw new ArgumentException("The parameter cannot be null or empty.", nameof(runTime));
+
+            if (releaseDate == default)
+                throw new ArgumentException("The parameter cannot be null or empty.", nameof(rating));
+
+            var cmdd = new CreateMovieDataDelegate(movieName, rating, runTime, releaseDate);
+            return executor.ExecuteNonQuery(cmdd);
+
         }
 
         public Movie FetchMovie(int movieId)
