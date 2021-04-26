3c56ea1cd7059b7d0ebffcf9b39e3f1ac5b34044
Tayler Stoppel <stoppel@ksu.edu>
2021-04-25 22:24:59 -0500

    Fixed Director Ratings SQL

--- CIS560Project/CIS560Project/Sql/Movies.DirectorRatings.sql
+++ CIS560Project/CIS560Project/Sql/Movies.DirectorRatings.sql
@@ -1,3 +1,4 @@
+CREATE OR ALTER PROCEDURE Movies.DirectorRatings AS
 WITH DirectorRanks(DirectorId, Rating, NumMovies) AS
 (
     SELECT DISTINCT D.DirectorId, SUM(MR.Score) OVER(PARTITION BY D.DirectorId), COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)
@@ -7,7 +8,7 @@ WITH DirectorRanks(DirectorId, Rating, NumMovies) AS
         LEFT JOIN Movies.MovieReview MR ON MR.MovieId = M.MovieId
 )
 
-SELECT D.FirstName, D.LastName, (DR.Rating / DR.NumMovies) AS DirectorRating,
+SELECT D.Firstname, D.FirstName, D.LastName, (DR.Rating / DR.NumMovies) AS DirectorRating,
 RANK() OVER(ORDER BY DR.Rating / DR.NumMovies DESC) AS Popularity
 FROM Movies.Director D
     RIGHT JOIN DirectorRanks DR ON DR.DirectorId = D.DirectorId
