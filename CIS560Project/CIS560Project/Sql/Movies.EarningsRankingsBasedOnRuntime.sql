38e5c655ad8dfc30935800172238c38ac2992a39
Tayler Stoppel <stoppel@ksu.edu>
2021-04-25 22:47:41 -0500

    Fixed Earnings procedure

--- CIS560Project/CIS560Project/Sql/Movies.EarningsRankingsBasedOnRuntime.sql
+++ CIS560Project/CIS560Project/Sql/Movies.EarningsRankingsBasedOnRuntime.sql
@@ -1,42 +1,18 @@
-WITH TheaterSales(MovieId, TicketSales) AS
+CREATE OR ALTER PROCEDURE Movies.EarningsRankingsBasedOnRuntime AS
+WITH TheaterSales(MovieId, TicketSales, MovieLength) AS
 (
-    SELECT DISTINCT M.MovieId, SUM(MT.TicketsSold * T.TicketPrice) OVER(PARTITION BY MT.MovieId) AS 'TicketsSold'
+    SELECT DISTINCT M.MovieId, SUM(MT.TicketsSold * T.TicketPrice) OVER(PARTITION BY MT.MovieId) AS 'TicketsSold',
+    CASE
+    WHEN M.RuntimeMinutes < 90 THEN 'The Movie is Less Than 90 Minutes'
+    WHEN M.RuntimeMinutes >= 90 AND M.RuntimeMinutes <= 120 THEN 'This Movie is Between 90 and 120 Minutes'
+    WHEN M.RuntimeMinutes >120 THEN 'This Movie is Longer Than 120 minutes'
+END AS MovieLength
     FROM Movies.Movie M
         JOIN Movies.MovieTheater MT ON MT.MovieId = M.MovieId
         JOIN Movies.Theater T ON T.TheaterId = MT.TheaterId
 )
 
-SELECT M.MovieTitle, TS.TicketSales,
-RANK() OVER(ORDER BY TS.TicketSales) AS 'Less than 90 minute movies'
+SELECT M.MovieTitle, TS.TicketSales, TS.MovieLength,
+RANK() OVER(PARTITION BY MovieLength ORDER BY TicketSales DESC) AS MovieRank
 FROM Movies.Movie M
     JOIN TheaterSales TS ON TS.MovieId = M.MovieId
-WHERE M.RuntimeMinutes < 90;
-GO
-
-WITH TheaterSales(MovieId, TicketSales) AS
-(
-    SELECT DISTINCT M.MovieId, SUM(MT.TicketsSold * T.TicketPrice) OVER(PARTITION BY MT.MovieId) AS 'TicketsSold'
-    FROM Movies.Movie M
-        JOIN Movies.MovieTheater MT ON MT.MovieId = M.MovieId
-        JOIN Movies.Theater T ON T.TheaterId = MT.TheaterId
-)
-SELECT M.MovieTitle, TS.TicketSales, 
-RANK() OVER(ORDER BY TS.TicketSales) AS 'Between 90 and 120 minute movies'
-FROM Movies.Movie M
-    JOIN TheaterSales TS ON TS.MovieId = M.MovieId
-WHERE M.RuntimeMinutes >= 90 AND M.RuntimeMinutes <= 120;
-GO
-
-WITH TheaterSales(MovieId, TicketSales) AS
-(
-    SELECT DISTINCT M.MovieId, SUM(MT.TicketsSold * T.TicketPrice) OVER(PARTITION BY MT.MovieId) AS 'TicketsSold'
-    FROM Movies.Movie M
-        JOIN Movies.MovieTheater MT ON MT.MovieId = M.MovieId
-        JOIN Movies.Theater T ON T.TheaterId = MT.TheaterId
-)
-SELECT M.MovieTitle, TS.TicketSales, M.RuntimeMinutes,
-RANK() OVER(ORDER BY TS.TicketSales) AS 'More than 120 minute movies'
-FROM Movies.Movie M
-    JOIN TheaterSales TS ON TS.MovieId = M.MovieId
-WHERE M.RuntimeMinutes > 120;
-GO
