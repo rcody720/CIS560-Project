ad14c19995ef0d75341fb44aad4eb2c465e3d164
Cody Reeves <rcody720@ksu.edu>
2021-04-25 23:04:22 -0500

    Categorizing

--- CIS560Project/CIS560Project/DataDelegates/GetEarningsRankingsBasedOnRuntimeDataDelegate.cs
+++ CIS560Project/CIS560Project/DataDelegates/GetEarningsRankingsBasedOnRuntimeDataDelegate.cs
@@ -20,11 +20,29 @@ namespace CIS560Project.DataDelegates
         public override Dictionary<string, double> Translate(SqlCommand command, IDataRowReader reader)
         {
             var movies = new Dictionary<string, double>();
-
+            double less90 = 0;
+            double middle = 0;
+            double greater120 = 0;
             while (reader.Read())
             {
-                movies.Add(reader.GetString("MovieLength"), reader.GetDouble("TicketSales"));
+                string s = reader.GetString("MovieLength");
+                if (s == "The Movie is Less Than 90 Minutes")
+                {
+                    less90 += reader.GetDouble("TicketSales");
+                }
+                else if (s == "This Movie is Between 90 and 120 Minutes")
+                {
+                    middle += reader.GetDouble("TicketSales");
+                }
+                else
+                {
+                    greater120 += reader.GetDouble("TicketSales");
+                }             
             }
+
+            movies.Add("The Movie is Less Than 90 Minutes", less90);
+            movies.Add("This Movie is Between 90 and 120 Minutes", middle);
+            movies.Add("This Movie is Longer Than 120 minutes", greater120);
 
             return movies;
         }
