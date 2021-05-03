861a98719c8e3318df55ec81f49a7dffa45d503b
Cody Reeves <rcody720@ksu.edu>
2021-05-03 14:34:48 -0500

    Adding comments

--- CIS560Project/CIS560Project/IMovieRepository.cs
+++ CIS560Project/CIS560Project/IMovieRepository.cs
@@ -30,7 +30,7 @@ namespace CIS560Project
         Movie GetMovie(string movieName);
 
         /// <summary>
-        /// Creates a new movie in the repository.
+        /// Creates a new movie in the database.
         /// </summary>
         /// <param name="movieId">Identifier of the movie.</param>
         /// <param name="movieName">Name of the movie.</param>
@@ -40,18 +40,60 @@ namespace CIS560Project
         /// <returns>The resulting instance of Movie.</returns>
         Movie CreateMovie(string movieName, string rating, int runTime, DateTime releaseDate);
 
+        /// <summary>
+        /// Gets the movies in order of current top scored
+        /// </summary>
+        /// <returns>Dictionary containing the movies and their respective score</returns>
         Dictionary<Movie, double> GetRecentMovies();
 
+        /// <summary>
+        /// Gets the movies along with their theater sales
+        /// </summary>
+        /// <returns>Dictionary containing the movies and their respective theater sales</returns>
         Dictionary<Movie, double> GetTheaterSales();
 
+        /// <summary>
+        /// Gets showtimes for a specific date
+        /// </summary>
+        /// <param name="currentDate">Specified date to show showtimes for</param>
+        /// <returns>List of the movie, the theater name, and the showtimes</returns>
         IReadOnlyList<(string, (string, string))> GetShowings(DateTime currentDate);
 
+        /// <summary>
+        /// Gets the earnings for 3 categories of runtimes, less than 90, between 90 & 120, and greater than 90.
+        /// </summary>
+        /// <returns>Dictionary containing the category of runtime and their respective earnings</returns>
+        Dictionary<string, double> GetEarningsRankingsBasedOnRuntime();
+
+        /// <summary>
+        /// Gets the movies of a specified genre
+        /// </summary>
+        /// <param name="genre">The specified genre</param>
+        /// <returns>List of the movies from the desired genre</returns>
         IReadOnlyList<Movie> GetMovies(string genre);
 
+        /// <summary>
+        /// Gets the movies that have a specified actor
+        /// </summary>
+        /// <param name="firstName">Actor's first name</param>
+        /// <param name="lastName">Actor's last name</param>
+        /// <returns>List of movies containing the specified actor</returns>
         IReadOnlyList<Movie> GetMovies(string firstName, string lastName);
 
+        /// <summary>
+        /// Fetchs the movies that have a specified director
+        /// </summary>
+        /// <param name="firstName">Director's first name</param>
+        /// <param name="lastName">Director's last name</param>
+        /// <returns>List of movies the specified director directed</returns>
         IReadOnlyList<Movie> FetchMovies(string firstName, string lastName);
 
+        /// <summary>
+        /// Creates a user review for a specific movie
+        /// </summary>
+        /// <param name="username">User leaving the review</param>
+        /// <param name="movieTitle">Title of the movie being reviewed</param>
+        /// <param name="score">The score the user is giving</param>
         void CreateUserReview(string username, string movieTitle, double score);
     }
 }
--- CIS560Project/CIS560Project/SqlMovieRepository.cs
+++ CIS560Project/CIS560Project/SqlMovieRepository.cs
@@ -9,16 +9,32 @@ using DataAccess;
 
 namespace CIS560Project
 {
+    /// <summary>
+    /// SqlMovieRepository class that implements IMovieRepository
+    /// </summary>
     public class SqlMovieRepository : IMovieRepository
     {
 
         private readonly SqlCommandExecutor executor;
 
+        /// <summary>
+        /// The constructor for SqlMovieRepository
+        /// </summary>
+        /// <param name="connectionString">string to connect to the database</param>
         public SqlMovieRepository(string connectionString)
         {
             executor = new SqlCommandExecutor(connectionString);
         }
 
+        /// <summary>
+        /// Creates a new movie in the database.
+        /// </summary>
+        /// <param name="movieId">Identifier of the movie.</param>
+        /// <param name="movieName">Name of the movie.</param>
+        /// <param name="rating">MPAA Rating of the movie.</param>
+        /// <param name="runTime">Run time of the movie in minutes.</param>
+        /// <param name="releaseDate">Date the movie is released.</param>
+        /// <returns>The resulting instance of Movie.</returns>
         public Movie CreateMovie(string movieName, string rating, int runTime, DateTime releaseDate)
         {
             if (string.IsNullOrWhiteSpace(movieName))
@@ -35,66 +51,120 @@ namespace CIS560Project
 
         }
 
+        /// <summary>
+        /// Fetches the movie with the given <paramref name="movieId"/> if it exists.
+        /// </summary>
+        /// <param name="movieId">Identifier of the movie to fetch.</param>
+        /// <returns> An instance of Movie containing the information of the requested movie.</returns>
         public Movie FetchMovie(int movieId)
         {
             var d = new FetchMovieDataDelegate(movieId);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Gets the movie with the given <paramref name="movieName"/> if it exists.
+        /// </summary>
+        /// <param name="movieName">Name of the movie to get.</param>
+        /// <returns>The movie containing the information of the requesetd movie.</returns>
         public Movie GetMovie(string movieName)
         {
             var d = new GetMovieDataDelegate(movieName);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Retrieves all movies from the database.
+        /// </summary>
+        /// <returns> <see cref="IReadOnlyList{T}"/> containing all movies.</returns>
         public IReadOnlyList<Movie> RetrieveMovies()
         {
             var d = new RetrieveMoviesDataDelegate();
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Gets the movies in order of current top scored
+        /// </summary>
+        /// <returns>Dictionary containing the movies and their respective score</returns>
         public Dictionary<Movie, double> GetRecentMovies()
         {
             var d = new GetRecentMoviesDataDelegate();
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Gets the movies along with their theater sales
+        /// </summary>
+        /// <returns>Dictionary containing the movies and their respective theater sales</returns>
         public Dictionary<Movie, double> GetTheaterSales()
         {
             var d = new GetTheaterSalesDataDelegate();
             return executor.ExecuteReader(d);
         }
 
-        public Dictionary<string, double> GetEarningsRankingsBasedOnRuntime()
-        {
-            var d = new GetEarningsRankingsBasedOnRuntimeDataDelegate();
-            return executor.ExecuteReader(d);
-        }
-
+        /// <summary>
+        /// Gets showtimes for a specific date
+        /// </summary>
+        /// <param name="currentDate">Specified date to show showtimes for</param>
+        /// <returns>List of the movie, the theater name, and the showtimes</returns>
         public IReadOnlyList<(string, (string, string))> GetShowings(DateTime currentDate)
         {
             var d = new GetShowingsDataDelegate(currentDate);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Gets the earnings for 3 categories of runtimes, less than 90, between 90 & 120, and greater than 90.
+        /// </summary>
+        /// <returns>Dictionary containing the category of runtime and their respective earnings</returns>
+        public Dictionary<string, double> GetEarningsRankingsBasedOnRuntime()
+        {
+            var d = new GetEarningsRankingsBasedOnRuntimeDataDelegate();
+            return executor.ExecuteReader(d);
+        }
+
+        /// <summary>
+        /// Gets the movies of a specified genre
+        /// </summary>
+        /// <param name="genre">The specified genre</param>
+        /// <returns>List of the movies from the desired genre</returns>
         public IReadOnlyList<Movie> GetMovies(string genre)
         {
             var d = new GetMoviesByGenreDataDelegate(genre);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Gets the movies that have a specified actor
+        /// </summary>
+        /// <param name="firstName">Actor's first name</param>
+        /// <param name="lastName">Actor's last name</param>
+        /// <returns>List of movies containing the specified actor</returns>
         public IReadOnlyList<Movie> GetMovies(string firstName, string lastName)
         {
             var d = new GetMoviesByActorDataDelegate(firstName, lastName);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Fetchs the movies that have a specified director
+        /// </summary>
+        /// <param name="firstName">Director's first name</param>
+        /// <param name="lastName">Director's last name</param>
+        /// <returns>List of movies the specified director directed</returns>
         public IReadOnlyList<Movie> FetchMovies(string firstName, string lastName)
         {
             var d = new GetMoviesByDirectorDataDelegate(firstName, lastName);
             return executor.ExecuteReader(d);
         }
 
+        /// <summary>
+        /// Creates a user review for a specific movie
+        /// </summary>
+        /// <param name="username">User leaving the review</param>
+        /// <param name="movieTitle">Title of the movie being reviewed</param>
+        /// <param name="score">The score the user is giving</param>
         public void CreateUserReview(string username, string movieTitle, double score)
         {
             if (string.IsNullOrWhiteSpace(username))
