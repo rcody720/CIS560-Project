194e52a68c59439d9fd0f8916aba00e376c11b18
Tayler Stoppel <stoppel@ksu.edu>
2021-05-03 22:26:43 -0500

    Added rank to director popularity query

--- CIS560Project/CIS560Project/Sql/Procedures/ReportQueries/Movies.DirectorRatings.sql
+++ CIS560Project/CIS560Project/Sql/Procedures/ReportQueries/Movies.DirectorRatings.sql
@@ -3,17 +3,23 @@
 partitioning them by the number of movies they've directed.*/
 
 CREATE OR ALTER PROCEDURE Movies.DirectorRatings AS
-WITH DirectorRanks(DirectorId, Rating, NumMovies) AS
+WITH DirectorRanks(DirectorId, Rating, NumMovies, DirectorStatus) AS
 (
-    SELECT DISTINCT D.DirectorId, SUM(MR.Score) OVER(PARTITION BY D.DirectorId), COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)
+    SELECT DISTINCT D.DirectorId, SUM(MR.Score) OVER(PARTITION BY D.DirectorId), (COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)) AS NumMovies,
+    Case
+        WHEN (COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)) > 10 THEN '1: Veteran Director'
+        WHEN (COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)) > 5 THEN '2: Experienced Director'
+        WHEN (COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)) > 1 THEN '3: Premering Director'
+        WHEN (COUNT(MD.MovieId) OVER(PARTITION BY D.DirectorId)) = 0 THEN '4: No Movies'
+    END AS DirectorStatus
     FROM Movies.Director D
         LEFT JOIN Movies.MovieDirector MD ON MD.DirectorId = D.DirectorId
         LEFT JOIN Movies.Movie M ON M.MovieId = MD.MovieId
         LEFT JOIN Movies.MovieReview MR ON MR.MovieId = M.MovieId
 )
 
-SELECT D.DirectorId, D.Firstname, D.FirstName, D.LastName, (DR.Rating / DR.NumMovies) AS DirectorRating,
-RANK() OVER(ORDER BY DR.Rating / DR.NumMovies DESC) AS Popularity
+SELECT D.DirectorId, D.Firstname, D.FirstName, D.LastName, (DR.Rating / DR.NumMovies) AS DirectorRating, DR.DirectorStatus,
+RANK() OVER(PARTITION BY DR.DirectorStatus ORDER BY DR.Rating / DR.NumMovies DESC) AS Popularity
 FROM Movies.Director D
     RIGHT JOIN DirectorRanks DR ON DR.DirectorId = D.DirectorId
-ORDER BY Popularity
+ORDER BY DirectorStatus
